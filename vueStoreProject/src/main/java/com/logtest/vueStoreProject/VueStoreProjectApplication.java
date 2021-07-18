d6939bb9ba99ef774fc883afbd5be33ad5f11d01
Matthaus <lucas_matthaus@outlook.com>
2021-07-18 09:39:00 -0300

    Empty Project

---
+++ vueStoreProject/src/main/java/com/logtest/vueStoreProject/VueStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.logtest.vueStoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueStoreProjectApplication.class, args);
+	}
+
+}
